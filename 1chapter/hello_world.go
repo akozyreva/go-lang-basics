7ac10dc23837a28f8955ed7d85d901473f74dbbf
Anna Kozyreva <a.kozyreva@comforte.com>
2025-12-07 17:41:54 +0100

    first commit

---
+++ 1chapter/hello_world.go
@@ -0,0 +1,11 @@
+// main - it's executive file
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("hello world")
+	fmt.Println("Hi, ", "my name is Ann", 32)
+	fmt.Print("No new line")
+	fmt.Print("Lala")
+}
---
+++ README.md
@@ -0,0 +1,13 @@
+# go-basics
+
+format file:
+
+```
+go fmt <path_to_file>
+```
+
+execute file:
+
+```
+go run <path_to_file>
+```
