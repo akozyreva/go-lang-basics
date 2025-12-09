2d18facf3ca96d29dc4c75730ea300b623c5b3fe
Anna Kozyreva <a.kozyreva@comforte.com>
2025-12-09 16:46:12 +0100

    add func sample

---
+++ 2chapter/printYear.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+	"time"
+)
+
+func main() {
+	// time.Now()
+	// time - package name
+	// Now - method, which can be exported. it returns time.Time object??
+	// time.Time -> Time is the type of package time
+	var now time.Time = time.Now()
+	var year int = now.Year()
+	fmt.Println(reflect.TypeOf(now))
+	fmt.Println(year)
+}
