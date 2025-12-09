93b9c438bcbd8b1e459e0e7ef854b7a55e6a3711
Anna Kozyreva <a.kozyreva@comforte.com>
2025-12-09 22:13:40 +0100

    add guess.go

---
+++ 2chapter/guess.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+
+	// math/rand - unique path to package. by convention rand is the name to call methods in package
+	"math/rand"
+)
+
+func main() {
+	var countsOfAttempts int = 10
+	var isGuessedNum bool = false
+	seconds := time.Now().Unix() // current time in int (Unix())
+	fmt.Println(seconds)
+	// rand.Seed is needed to generate real random number
+	rand.Seed(seconds)
+	target := rand.Intn(100) + 1
+	fmt.Println(target)
+	fmt.Println("I've chosen a random num between 1 and 100")
+	fmt.Println("Can you guess it?")
+	for i := countsOfAttempts; i > 0; i-- {
+		fmt.Println("You have :", i, "attempts")
+		reader := bufio.NewReader(os.Stdin)
+		input, _ := reader.ReadString('\n')
+		input = strings.TrimSuffix(input, "\n")
+		inputNum, err := strconv.Atoi(input)
+		if err != nil {
+			log.Fatal(err)
+		}
+		if inputNum == target {
+			fmt.Println("Your guess was correct. End")
+			isGuessedNum = true
+			break
+		} else {
+			fmt.Println("Ooops.. try again")
+		}
+	}
+	if !isGuessedNum {
+		fmt.Println("You lost. Correct number is: ", target)
+	}
+
+}
