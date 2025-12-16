383a234a708931d72e56b844921c36122027538f
Anna Kozyreva <kozyreva.hanna@gmail.com>
2025-12-16 22:23:24 +0100

    add more explanations about pkg

--- 4chapterPkgSample/README.md
+++ 4chapterPkgSample/README.md
@@ -9,3 +9,8 @@ import in `main.go` is:
 ```
 import "4chapterPkgSample/greeting"
 ```
+
+package naming convention:
+
+- starts with small letter and short word if possible: `fmt`
+- use 1 word, if not possible, write it all as one: `strconv`
--- 4chapterPkgSample/greeting/greeting.go
+++ 4chapterPkgSample/greeting/greeting.go
@@ -3,10 +3,17 @@ package greeting
 import "fmt"
 
 // Hello this method is for import, so it starts from capital letter
+// only functions with capital letters are being exported
 func Hello() {
 	fmt.Println("Hello")
 }
 
 func Hi() {
 	fmt.Println("Hi")
+}
+
+func AllGreetings() {
+	// I can call them directly - without this or anythgin
+	Hello()
+	Hi()
 }
