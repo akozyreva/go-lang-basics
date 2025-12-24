91641f5ff2500037ace489dcf78863d71d21d37b
Anna Kozyreva <kozyreva.hanna@gmail.com>
2025-12-24 20:33:35 +0100

    add more samples to slices

--- 6chapterSlices/severalArgsInFunction.go
+++ 6chapterSlices/severalArgsInFunction.go
@@ -11,4 +11,7 @@ func severalInt(numbers ...int) { // ...int - it's actually slice!
 func main() {
 	severalInt(1)
 	severalInt(1, 2, 3)
+	slice := []int{3, 2, 1}
+	severalInt(slice...) // this syntax is used to unpack slice
+	//severalInt(slice) -> err, because we're waiting for values, not for slice
 }
