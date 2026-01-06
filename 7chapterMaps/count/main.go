b30e11e99d5d4d1d8ac5d530494d96039116e036
Anna Kozyreva <kozyreva.hanna@gmail.com>
2026-01-06 17:16:52 +0100

    add counter of votes and unique names

--- 7chapterMaps/count/main.go
+++ 7chapterMaps/count/main.go
@@ -12,4 +12,33 @@ func main() {
 		log.Fatal(err)
 	}
 	fmt.Println(lines)
+	// count the number of votes
+	var names []string
+	var counts []int
+	for _, name := range lines {
+		isNameFound := false
+		for _, sortedName := range names {
+			if name == sortedName {
+				isNameFound = true
+				break
+			}
+		}
+		if !isNameFound {
+			names = append(names, name)
+		}
+	}
+	for _, sortedName := range names {
+		count := 0
+		for _, name := range lines {
+			if name == sortedName {
+				count++
+			}
+		}
+		counts = append(counts, count)
+	}
+	fmt.Println(names)
+	fmt.Println(counts)
+	for i, name := range names {
+		fmt.Printf("%s: %d\n", name, counts[i])
+	}
 }
